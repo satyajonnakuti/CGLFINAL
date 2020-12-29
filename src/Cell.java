3262c052e9390c7ae9a7f9e92325d6a487a8baf2
satyajonnakuti <satyajonnakuti@msitprogran.net>
2020-12-29 08:46:47 +0530

    added back files

---
+++ src/Cell.java
@@ -0,0 +1,23 @@
+
+
+public class Cell {
+	/*This specifies the row and column of this particular cell in
+	 * the game board
+	 * 
+	 */
+	public int row;
+	public int column;
+	/*This method return a boolean value 
+	 * if true indicates the cell is alive
+	 * if false indicated the cell is dead
+	 */
+	public Cell(int row,int column)
+	{
+		this.row=row;
+		this.column=column;
+	}
+	public boolean isAlive()
+	{
+		
+	}
+}
---
+++ src/TestClass.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ *
+ */
+public class CGL {
+
+	/**
+	 * @param args
+	 */
+	public Board b;
+	
+	/*In main method ,the scanner class will take the input for the game 
+	 * 
+	 * 
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
